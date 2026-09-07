da1b335e1f269889f2a6628fdfc45e2b4aec7276
Gulnawaj <gulnawaj001646@gmail.com>
2026-09-07 22:15:14 +0530

    interview panel logic corrected

--- takehome-03-hiring-pipeline/backend/src/models/InterviewPanel.js
+++ takehome-03-hiring-pipeline/backend/src/models/InterviewPanel.js
@@ -6,18 +6,32 @@ const InterviewPanelSchema = new mongoose.Schema({
     ref: 'Application',
     required: true,
   },
+
   interviewer_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
     index: true,
   },
+
   assigned_at: {
     type: Date,
     default: Date.now,
-  }
+  },
+
+  // Date and time when the interview is actually scheduled.
+  scheduled_at: {
+    type: Date,
+    default: null,
+  },
 });
 
-InterviewPanelSchema.index({ application_id: 1, interviewer_id: 1 }, { unique: true });
+InterviewPanelSchema.index(
+  { application_id: 1, interviewer_id: 1 },
+  { unique: true }
+);
 
-export const InterviewPanel = mongoose.model('InterviewPanel', InterviewPanelSchema);
+export const InterviewPanel = mongoose.model(
+  'InterviewPanel',
+  InterviewPanelSchema
+);
